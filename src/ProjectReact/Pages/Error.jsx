e478a36e57f11bef7ccb569cbde44ce7e8b54ee7
RahulChakraborty020 <rahulchakraborty020@gmail.com>
2024-02-07 23:58:41 +0530

    project

--- src/ProjectReact/Pages/Error.jsx
+++ src/ProjectReact/Pages/Error.jsx
@@ -4,50 +4,50 @@ import { Container } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-const Error =()=>{
-    return(
+const Error = () => {
+    return (
 
         <>
-        <div className='bg-black w-100'>
-            <Container>
+            <div className='bg-black w-100'>
+                <Container>
 
-                <Row className='mt-5 mb-5'>
-                    <Col></Col>
-                </Row>
-                <Row className='mt-5 mb-5'>
-                    <Col></Col>
-                </Row>
-                <Row className='mt-5 mb-5'>
-                    <Col></Col>
-                </Row>
-                <Row className='mt-5 mb-5'>
-                    <Col></Col>
-                </Row>
+                    <Row className='mt-5 mb-5'>
+                        <Col></Col>
+                    </Row>
+                    <Row className='mt-5 mb-5'>
+                        <Col></Col>
+                    </Row>
+                    <Row className='mt-5 mb-5'>
+                        <Col></Col>
+                    </Row>
+                    <Row className='mt-5 mb-5'>
+                        <Col></Col>
+                    </Row>
 
 
-                <Row className='mt-5'>
-                    <Col className='col-4'></Col>
-                    <Col className='col-6 text-light fs-3 fw-bold'>
-                    Something went wrong!
-                    </Col>
-                </Row>
+                    <Row className='mt-5'>
+                        <Col className='col-4'></Col>
+                        <Col className='col-6 text-light fs-3 fw-bold'>
+                            Something went wrong!
+                        </Col>
+                    </Row>
 
-                <Row className='mt-2'>
-                    <Col className='col-4'></Col>
-                    <Col className='col-6 text-light fs-6 fw-bold'>
-                    Internal server error, please try again later.
-                    </Col>
-                </Row>
+                    <Row className='mt-2'>
+                        <Col className='col-4'></Col>
+                        <Col className='col-6 text-light fs-6 fw-bold'>
+                            Internal server error, please try again later.
+                        </Col>
+                    </Row>
 
-                <Row className='mt-5 pb-5'>
-                <Col className='col-5'></Col>
-                    <Col className='col-6 ps-md-4'>
-                   <Link to='/' className='custom-go fw-bold'>Go To Home</Link>
-                    </Col>
-                </Row>
+                    <Row className='mt-5 pb-5'>
+                        <Col className='col-5'></Col>
+                        <Col className='col-6 ps-md-4'>
+                            <Link to='/' className='custom-go fw-bold'>Go To Home</Link>
+                        </Col>
+                    </Row>
 
-            </Container>
-        </div>
+                </Container>
+            </div>
         </>
     )
 }
